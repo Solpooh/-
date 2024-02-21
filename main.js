fc07c3a0a44ada6886f472ba2f5225cbebd50b9c
Solpooh <hsw9420@naver.com>
2024-02-21 16:43:21 +0900

    [feat] 장바구니 수량증가기능

--- main.js
+++ main.js
@@ -33,6 +33,8 @@ let shopItemsData = [
     }
 ];
 
+let basket = [];
+
 let generateShop = () => {
     return shop.innerHTML = shopItemsData
     .map((x) => {
@@ -45,14 +47,37 @@ let generateShop = () => {
             <div class="price-quantity">
                 <h2>$ ${price}</h2>
                 <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
-                    <div class="quantity">0</div>
-                    <i class="bi bi-plus-lg"></i>
+                    <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                    <div id=${id} class="quantity">0</div>
+                    <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
         </div>
     </div>`
-    }).join("");
+    })
+    .join("");
 }
 
 generateShop();
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    // 물건이 존재하지 않으면 새로 push, 존재하면 수량만 증가 
+    if(search === undefined) {
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    } else {
+        search.item += 1;
+    }
+
+    console.log(basket);
+};
+let decrement = (id) => {
+    let selectedItem = id;
+    console.log(selectedItem.id);
+};
+let update = () => {};
